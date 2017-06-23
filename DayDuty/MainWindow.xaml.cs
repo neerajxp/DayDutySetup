ca8cfb0b9a9bcdc86638d5762775d6c53b9f6df8
Neeraj Maurya <neerajxp@gmail.com>
2017-06-22 22:09:30 -0400

    first commit

---
+++ DayDuty/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DayDuty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_DB_Click(object sender, RoutedEventArgs e)
+        {
+            //Get full folder path where database file exists using the relative path if required
+            string dbpath = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"MyDatabase"));
+            //Register this path in DataDirectory
+            AppDomain.CurrentDomain.SetData("DataDirectory", dbpath);
+
+            SqlConnection cn = new SqlConnection();
+            //Use the DAtaDirectory path to connect to database
+            cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\mydb.mdf;Integrated Security=True;Connect Timeout=30";
+            cn.Open();
+            MessageBox.Show(string.Format("Connection status is -> {0}", cn.State.ToString()));
+        }
+    }
+}
